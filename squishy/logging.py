fa1a39615b3d955a2aa45d0f5fc0c366c0db5912
Travis Mehlinger <tmehlinger@gmail.com>
2016-09-24 15:42:14 -0500

    removed more 2.6 cruft

--- squishy/logging.py
+++ squishy/logging.py
@@ -1,18 +1,9 @@
 from __future__ import absolute_import
 
 import logging
-import sys
-
-
-if sys.version_info[:2] == (2, 6):
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-else:
-    from logging import NullHandler
 
 
 def get_logger(name, handler=None):
     logger = logging.getLogger(name)
-    logger.addHandler(handler or NullHandler())
+    logger.addHandler(handler or logging.NullHandler())
     return logger
