ee539ad8eff8cb934db707b9dee95bd79384e85c
Travis Mehlinger <tmehlinger@gmail.com>
2016-09-24 07:54:31 -0500

    fix coveralls

--- run_coveralls.py
+++ run_coveralls.py
@@ -4,5 +4,6 @@ from subprocess import call
 
 if __name__ == '__main__':
     if 'CIRCLECI' in os.environ:
+        print('running coveralls')
         rc = call('coveralls')
         raise SystemExit(rc)
